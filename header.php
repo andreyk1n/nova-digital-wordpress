32da08c9f826fd8724b47e476f6e7c0bbc17871b
andreyk1n <betas23wr@gmail.com>
2026-05-15 13:57:20 +0300

    upd header

--- header.php
+++ header.php
@@ -1,16 +1,11 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-
 <?php get_template_part('template-parts/partials/head'); ?>
-
 <body <?php body_class(); ?>>
-
 	<?php wp_body_open(); ?>
-
 	<header class="header">
 		<div class="container header__container">
 			<a href="#hero" class="header__logo">Nova Digital</a>
-
 			<nav class="header__nav nav" aria-label="Main navigation">
 				<ul class="nav__list">
 					<li class="nav__item"><a class="nav__link" href="#about">About us</a></li>
@@ -21,7 +16,6 @@
 					<li class="nav__item"><a class="nav__link" href="#contacts">Contacts</a></li>
 				</ul>
 			</nav>
-
 			<div class="header__actions">
 				<button class="theme-toggle" aria-label="Switch theme">
 					☼
