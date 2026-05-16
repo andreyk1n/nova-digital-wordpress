f4309459af0d824031f376fd0cf3c3874c38833a
andreyk1n <betas23wr@gmail.com>
2026-05-16 11:23:30 +0300

    upd footer

--- footer.php
+++ footer.php
@@ -1,18 +1,30 @@
 <footer class="footer">
 	<?php $logo = get_field('logo', 'option'); ?>
+	<?php $footer_description = get_field('footer_description', 'option'); ?>
+	<?php $footer_label = get_field('footer_label', 'option'); ?>
+	<?php $footer_copyright = get_field('footer_copyright', 'option'); ?>
+	<?php $footer_socials = get_field('footer_socials', 'option'); ?>
 	<div class="container">
 		<div class="footer__top">
 			<div>
 				<div class="footer__logo"><?php echo $logo; ?></div>
-				<p class="footer__description">
-					Creative digital marketing agency focused on growth, branding and performance.
-				</p>
+				<?php if ($footer_description): ?>
+					<p class="footer__description">
+						<?php echo $footer_description; ?>
+					</p>
+				<?php endif; ?>
 			</div>
 			<?php get_template_part('template-parts/partials/footer-nav'); ?>
 		</div>
 		<div class="footer__bottom">
-			<p>© 2026 Nova Digital. All rights reserved.</p>
-			<p>Designed for performance & accessibility.</p>
+			<p>©
+				<?php echo date('Y'); ?> <?php echo $footer_copyright; ?>
+			</p>
+			<?php if ($footer_label): ?>
+				<p>
+					<?php echo $footer_label; ?>
+				</p>
+			<?php endif; ?>
 		</div>
 	</div>
 </footer>
